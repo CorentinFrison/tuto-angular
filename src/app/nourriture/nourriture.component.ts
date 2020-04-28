a11340ae948efc5bb622c6660ab111688fef325f
CorentinFrison <corentinfrison@gmail.com>
2020-04-28 14:11:46 +0200

    fix nourriture component

--- src/app/nourriture/nourriture.component.ts
+++ src/app/nourriture/nourriture.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NourritureService } from '../services/nourriture.service';
 
 @Component({
   selector: 'app-nourriture',
@@ -7,12 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NourritureComponent implements OnInit {
 
-  constructor() { }
+  plats: any[];
 
-  ngOnInit(): void {
+  constructor(private nourritureService: NourritureService) { }
+
+  ngOnInit() {
+    this.plats = this.nourritureService.nourritures;
   }
 
 }
-ngOnInit() {
-  this.nourritures = this.nourritureService.nourriture;
-}
+
