04de868adee13e08d0a54a11091aac7b065c1e5e
Cloelh <cloelh@outlook.fr>
2020-04-29 11:33:40 +0200

    panier

--- src/app/panier-view/panier-view.component.ts
+++ src/app/panier-view/panier-view.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { NourritureService } from '../services/nourriture.service';
 
-
 @Component({
-  selector: 'app-panier-view',
-  templateUrl: './panier-view.component.html',
-  styleUrls: ['./panier-view.component.css']
+  selector: 'app-panier',
+  templateUrl: './panier.component.html',
+  styleUrls: ['./panier.component.css']
 })
 export class PanierViewComponent implements OnInit {
 
-  nourriture: any[];
+  panierList:any[];
 
   @Input() platName: string;
   @Input() platDescription: string;
@@ -21,10 +20,13 @@ export class PanierViewComponent implements OnInit {
   @Input() id: number;
   @Input() panier: boolean;
 
+
   constructor(private nourritureService: NourritureService) { }
 
+
   ngOnInit() {
-    this.nourriture = this.nourritureService.nourritures;
+    this.panierList = this.nourritureService.nourritures;
   }
+
 
 }
